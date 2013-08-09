8131acdc56ef673ccb7973ae4f1b1fafed0b9eec
sshekh <sauravshekhar01@gmail.com>
2013-08-09 12:48:03 +0530

    Added test codes in C,C++,python

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	cin>>i;
+	while(i!=42){
+	    cout<<i;
+	    cin>>i;
+	}
+	return 0;
+}
---
+++ test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+n = int(raw_input("Enter the numbers"))
+while n!= 42:
+    print n
+    n = int(raw_input())
