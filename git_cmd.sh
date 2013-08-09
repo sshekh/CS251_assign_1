5f6c1204a892caee5e92b2e480a8ca4d60d3e6ea
Saurav Shekhar <sauravshekhar01@gmail.com>
2013-08-10 03:19:40 +0530

    added Command Line history

---
+++ git_cmd.sh
@@ -0,0 +1,118 @@
+git
+clear
+git clone https://github.com/sshekh/Advanced-Audio_Equalizer.git
+cd Advanced-Audio_Equalizer
+git add .
+gitk --all
+sudo apt-get install gitk
+gitk --all
+git log --oneline --graph --decorate --all
+git add src
+cd src
+git add Final.java
+git add Pan.java
+cd ..
+sudo git add .
+git config core.autocrlf
+git config core.autocrlf = false
+git config core.autocrlf
+git config core.autocrlf false
+git config core.autocrlf
+git add .
+git commit git add .
+git add .
+git commit -m "added all files"
+git push -u origin master
+gitk --all
+python firewall.pyc
+cd /media/saurav
+ls
+cd DATA/prog/lang/Android
+git clone https://github.com/sshekh/Android-Joystick
+cd Android_Joystick
+git add .
+cd ..
+cd Android-Joystick
+cd ..
+cd ...
+cd .
+cd /media/saurav/DATA/prog/lang/Android/
+cd Android-Joystick
+git add .
+git commit auto.crlf false
+git config auto.crlf false
+git add .
+git config core.autocrlf false
+git add .
+git commit -m "added all files"
+git push -u origin master
+
+man git
+cd prog
+cd lang/git
+sudo apt-add-repository --remove  ppa:fs-icons-ubuntu/ppa 
+sudo apt-get update
+exit
+cd prog/lang/git
+ls
+git init CS251_assign_1
+cd CS251_assign_1
+git remove CS251_assign_1
+git --help
+cd ..
+git rm CS251_assign_1
+git rm CS251_assign_1.git
+git init CS251_assign_1
+cd CS251_assign_1
+git checkout --help
+git add .
+git log --graph --decorate --abbrev-commit
+git log --graph --decorate --abbrev-commit --all --pretty=online
+git user.name
+git user.name --help
+git commit -m "Added test codes in C,C++,python"
+haskell
+man haskell
+man hpc
+man ghc
+man fpc
+man gfortran
+git add README
+git commit -m "Added README"
+git log -4
+git config --global user.name "Saurav Shekhar"
+git log -2
+git branch --help
+git branch shekhar_edit
+git branch
+git checkout shekhar_edit
+git log -1 --author=sshekh
+git log -1 --pretty=medium
+git log -2 --stat
+git log -2 -p
+git master...shekhar_edit
+git log master...shekhar_edit
+
+git merge --help
+git --version
+ls -al
+git --tree | more
+tree .git | more
+git tree .git | more
+git status
+git
+git remote add origin https://github.com/sshekh/CS251_assign_1
+git add git_commands.sh
+git commit -m "Added Stanford git overview"
+git push -u origin master
+git update-server-inf
+mv README README.md
+
+git push -u origin shekhar_edit
+git merge shekhar_edit
+git log
+git push -u origin shekhar_edit
+git branch
+cut -c 8 - 25 `history` > git_cmd.sh
+cut -c 8-25 `history` > git_cmd.sh
+cut -c 8-25 | history > git_cmd.sh
