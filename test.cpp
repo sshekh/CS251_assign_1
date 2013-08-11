1d96ebdf5d0002d9056e2fe94b700c4e9e895377
Saurav Shekhar <sauravshekhar01@gmail.com>
2013-08-12 00:08:50 +0530

    Added comments to test.cpp

--- test.cpp
+++ test.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
+// declaring the namespace
 using namespace std;
 
 int main()
 {
 	int i;
-	cin>>i;
-	while(i!=42){
-	    cout<<i;
+	cin>>i;	//taking i as input
+	while(i!=42){	
+	    cout<<i<<"\n";	//print i onto screen in a line and input it again
 	    cin>>i;
 	}
 	return 0;
